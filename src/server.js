9dffa082393d76d6b7dbb6a312f587999dbbb047
besupernow <besupernowmchs@outlook.com>
2024-01-16 22:28:49 -0500

    first commit

--- src/server.js
+++ src/server.js
@@ -95,23 +95,29 @@ class ChildServer {
             if (!files.Contents) {
                 throw new Error('Folder not found or is empty');
             }
+            const totalSize = files.Contents.reduce((acc, obj) => acc + (obj.Size || 0), 0);
+            let downloadedSize = 0;
+
             for (const object of files.Contents) {
-                log.info(object.Key)
+                log.info(`Downloading ${object.Key}`)
+                if (object.Key.endsWith('/')) {
+                    continue; // Skip if it's a folder entry
+                }
                 const getObjectCommand = new GetObjectCommand({
                     Bucket: "galacticstudio",
                     Key: object.Key
                 });
-                if(object.Key.endsWith("/")){
-                    log.info("Is dir")
-                    continue
-                }
-                const dir = path.join('servers', `${this.game}-${this.port}`);
+                const localPath = object.Key.split('/').slice(2).join('/');
+                const dir = path.join('servers', `${this.game}-${this.port}`, path.dirname(localPath));
                 if (!fs.existsSync(dir)){
                     fs.mkdirSync(dir, { recursive: true });
                 }
                 const { Body } = await s3Client.send(getObjectCommand);
                 const pipeline = promisify(stream.pipeline);
                 await pipeline(Body, fs.createWriteStream(`${dir}/${path.basename(object.Key)}`));
+                downloadedSize += object.Size || 0;
+                const percentComplete = (downloadedSize / totalSize * 100).toFixed(2);
+                console.log(`Download progress: ${percentComplete}%`);
             }
             resolve()
         }).catch(err=>{
